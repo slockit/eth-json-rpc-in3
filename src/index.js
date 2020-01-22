26fa7ef22c21e03c8521260222312142f9142aea
chirag-parmar <chirag.parmar@slock.it>
2020-01-22 12:30:13 +0100

    minimal code

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const In3Wasm = require('in3-wasm')
+const { errors: rpcErrors } = require('eth-json-rpc-errors')
+const createAsyncMiddleware = require('json-rpc-engine/src/createAsyncMiddleware')
+
+module.exports = createIn3Middleware
+
+function createIn3Middleware (config = {}) {
+
+  //use default values if the values don't exist
+  config["chainId"] = config.chainId || 'mainnet'
+  config["signatureCount"] = config.signatureCount || 0
+  config["maxAttempts"] = config.maxAttempts || 5
+  config["proof"] = config.proof || 'standard'
+  config["keepIn3"] = false
+
+  //create a new client object
+  const in3 = new In3Wasm(config)
+
+  return createAsyncMiddleware(async (req, res, next) => {
+
+    const in3Res = await in3.sendRPC(req.method, req.params)
+
+    if (in3Res.error) throw rpcErrors.internal(in3Res.error.toString(), in3Res.error)
+
+    // set result and exit retry loop
+    res.result = in3Res;
+    return
+  })
+}
