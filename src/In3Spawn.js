e6e8c6a7dc605d5acd3fc54ea324ab3887d51905
chirag-parmar <chirag.parmar@slock.it>
2020-02-10 15:20:57 +0100

    allow setting config on subsequent spawns

--- src/In3Spawn.js
+++ src/In3Spawn.js
@@ -6,8 +6,6 @@ module.exports = In3Spawn;
 const mainnetInstance = new IN3asm({ chainId: "mainnet", ... defaultIn3Config});
 const kovanInstance = new IN3asm({ chainId: "kovan", ... defaultIn3Config});
 const goerliInstance = new IN3asm({ chainId: "goerli", ... defaultIn3Config});
-const rinkebyInstance = new IN3asm({ chainId: "rinkeby", ... defaultIn3Config});
-const ropstenInstance = new IN3asm({ chainId: "ropsten", ... defaultIn3Config});
 
 function In3Spawn(config = {}) {
   if (!config.chainId) {
@@ -15,14 +13,13 @@ function In3Spawn(config = {}) {
   }
 
   if (config.chainId == "kovan") {
+    kovanInstance.setConfig(config);
     return kovanInstance;
   } else if (config.chainId == "goerli") {
+    goerliInstance.setConfig(config);
     return goerliInstance;
-  } else if (config.chainId == "ropsten") {
-    return ropstenInstance;
-  } else if (config.chainId == "rinkeby") {
-    return rinkebyInstance;
   } else if (config.chainId == "mainnet") {
+    mainnetInstance.setConfig(config);
     return mainnetInstance;
   }
 }
