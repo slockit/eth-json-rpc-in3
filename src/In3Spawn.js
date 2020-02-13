9920e6758ec32fdf047d0403900e737526735efe
chirag-parmar <chirag.parmar@slock.it>
2020-02-13 11:35:42 +0100

    agnostic chainId format

--- src/In3Spawn.js
+++ src/In3Spawn.js
@@ -7,18 +7,28 @@ const mainnetInstance = new IN3asm({ chainId: "mainnet", ... defaultIn3Config});
 const kovanInstance = new IN3asm({ chainId: "kovan", ... defaultIn3Config});
 const goerliInstance = new IN3asm({ chainId: "goerli", ... defaultIn3Config});
 
+function checkChainId(unknownId, chainName) {
+  const chainIdMap = {
+    kovan: ["kovan", 42, "42", "0x2a", "2a", "2A"],
+    mainnet: ["mainnet", 1, "0x1", "1"],
+    goerli: ["goerli", 5, "0x5", "5"]
+  }
+
+  return chainIdMap[chainName].includes(unknownId)
+}
+
 function In3Spawn(config = {}) {
   if (!config.chainId) {
     throw new Error("config has no property chainId");
   }
 
-  if (config.chainId == "kovan") {
+  if (checkChainId(config.chainId, "kovan")) {
     kovanInstance.setConfig(config);
     return kovanInstance;
-  } else if (config.chainId == "goerli") {
+  } else if (checkChainId(config.chainId, "goerli")) {
     goerliInstance.setConfig(config);
     return goerliInstance;
-  } else if (config.chainId == "mainnet") {
+  } else if (checkChainId(config.chainId, "mainnet")) {
     mainnetInstance.setConfig(config);
     return mainnetInstance;
   }
